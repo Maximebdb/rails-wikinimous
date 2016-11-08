c2fdbd1a5afd22ef67cdcec88dc7373a432dce90
Maxime BDB <maxime@packyourskills.com>
2016-11-08 18:17:35 +0100

    end of the day

--- Gemfile
+++ Gemfile
@@ -29,7 +29,11 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
+#use Faker
 gem 'faker', '~> 1.6', '>= 1.6.6'
+#use Kramdown
+gem 'kramdown', '~> 1.12'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles, only: [:new, :update, :show, :edit, :create, :index, :destroy]
+
+  # get    "articles",          to: "articles#index"
+
+  # get    "articles/new",      to: "articles#new"
+  # post   "articles",          to: "articles#create"
+
+  # get    "articles/:id",      to: "articles#show"
+
+  # get    "articles/:id/edit", to: "articles#edit"
+  # patch  "articles/:id",      to: "articles#update"
+
+  # delete "articles/:id",      to: "articles#destroy"
 end
+
+
